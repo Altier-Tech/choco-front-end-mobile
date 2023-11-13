43fdae50a1169401a78ee626b6c8e1d779737e19
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-13 20:12:33 +0530

    Update chat_page.dart

--- lib/chat/chat_page.dart
+++ lib/chat/chat_page.dart
@@ -1,7 +1,7 @@
 import 'package:chatgpt_client/api/chat_api.dart';
 import 'package:chatgpt_client/models/chat_message.dart';
 import 'package:chatgpt_client/widgets/message_bubble.dart';
-import 'package:chatgpt_client/widgets/message_composer.dart';
+import 'message_composer.dart'
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
