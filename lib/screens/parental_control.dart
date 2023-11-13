fc6aa633ba0df633d3206b7fc7264507e1d53a97
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-13 20:22:23 +0530

    Update parental_control.dart

--- lib/screens/parental_control.dart
+++ lib/screens/parental_control.dart
@@ -1,3 +1,4 @@
+import 'package:choco/gpt/chat_api.dart';
 import 'package:flutter/material.dart';
 
 import '../models/parental_control_items.dart';
@@ -12,6 +13,8 @@ class ParentalControl extends StatefulWidget {
 }
 
 class _ParentalControlState extends State<ParentalControl> {
+  ChatApi chatApi = ChatApi();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +27,8 @@ class _ParentalControlState extends State<ParentalControl> {
           mainAxisAlignment: MainAxisAlignment.start,
           //crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(padding: EdgeInsets.symmetric(horizontal: 30.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
               child: Container(
                 width: 350,
                 height: 200,
@@ -45,69 +49,96 @@ class _ParentalControlState extends State<ParentalControl> {
                 ),
               ),
             ),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 20.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  CircleShortcut(name: "Block/Unblock\nSites", imageUrl: 'block_sites.svg',circleColor: Colors.red,onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SiteBlockerScreen(),
-                      ),
-                    );
-                  },
+                  CircleShortcut(
+                    name: "Block/Unblock\nSites",
+                    imageUrl: 'block_sites.svg',
+                    circleColor: Colors.red,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SiteBlockerScreen(),
+                        ),
+                      );
+                    },
                   ),
-                  CircleShortcut(name: "Choco\nChat", imageUrl: 'chat.png',circleColor: Colors.grey,onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChatScreen(),
-                      ),
-                    );
-                  },
+                  CircleShortcut(
+                    name: "Choco\nChat",
+                    imageUrl: 'chat.png',
+                    circleColor: Colors.grey,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatScreen(),
+                        ),
+                      );
+                    },
                   ),
-                  CircleShortcut(name: "Intelligent\nService", imageUrl: 'intelligent_img.png',circleColor: Colors.greenAccent,onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => YourDestinationScreen(),
-                    //   ),
-                    // );
-                  },
+                  CircleShortcut(
+                    name: "Intelligent\nService",
+                    imageUrl: 'intelligent_img.png',
+                    circleColor: Colors.greenAccent,
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => YourDestinationScreen(),
+                      //   ),
+                      // );
+                    },
                   ),
                 ],
               ),
             ),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  CircleShortcut(name: "Choco\nEngine", imageUrl: 'Choco_engine_img.png',onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => YourDestinationScreen(),
-                    //   ),
-                    // );
-                  },
+                  CircleShortcut(
+                    name: "Choco\nEngine",
+                    imageUrl: 'Choco_engine_img.png',
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => YourDestinationScreen(),
+                      //   ),
+                      // );
+                    },
                   ),
-                  CircleShortcut(name: "Setup Child\nProfile", imageUrl: 'child_profile_img.png',circleColor: Colors.yellow,onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => YourDestinationScreen(),
-                    //   ),
-                    // );
-                  },),
-                  CircleShortcut(name: "Browser\nSettings", imageUrl: 'browser_settings_img.png',circleColor: Colors.grey,onTap: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => YourDestinationScreen(),
-                    //   ),
-                    // );
-                  },),
+                  CircleShortcut(
+                    name: "Setup Child\nProfile",
+                    imageUrl: 'child_profile_img.png',
+                    circleColor: Colors.yellow,
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => YourDestinationScreen(),
+                      //   ),
+                      // );
+                    },
+                  ),
+                  CircleShortcut(
+                    name: "Browser\nSettings",
+                    imageUrl: 'browser_settings_img.png',
+                    circleColor: Colors.grey,
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => YourDestinationScreen(),
+                      //   ),
+                      // );
+                    },
+                  ),
                 ],
               ),
             ),
