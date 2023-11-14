cb2bfabc546a9a58a026b7d538f682a1e1c10954
Thilina Dilshan <tilinadilshan1999@gmail.com>
2023-11-15 02:57:39 +0530

    add color and assets

--- lib/models/shortcuts.dart
+++ lib/models/shortcuts.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 
 class CircleShortcut extends StatelessWidget {
   final String name;
-  final String imageUrl;
+  final String imagePath;
+  final Color circleColor; // New parameter for circle color
+  final Color textColor;  // New parameter for text color
 
-  CircleShortcut({required this.name, required this.imageUrl});
+  CircleShortcut({
+    required this.name,
+    required this.imagePath,
+    this.circleColor = Colors.blue, // Default circle color
+    this.textColor = Colors.black, // Default text color
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
-          width: 50, // Adjust the size as needed
+          width: 50,
           height: 50,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: Colors.blue, // You can set your own color
+            color: circleColor, // Use the provided circle color
           ),
-          // You can add your icon or text or any widget inside the circle
           child: CircleAvatar(
-            backgroundImage: NetworkImage(imageUrl),
+            backgroundImage: AssetImage(imagePath),
             backgroundColor: Colors.transparent,
           ),
         ),
-        SizedBox(height: 8), // Adjust spacing between circle and name
-        Text(name),
+        SizedBox(height: 8),
+        Text(
+          name,
+          style: TextStyle(color: textColor), // Use the provided text color
+        ),
       ],
     );
   }
