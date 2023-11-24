20c0f192feb9da212694848bdf437f0bb57a1fd8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-24 19:04:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,19 @@
+import 'package:choco/splash%20screen/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:choco/screens/home_screen.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
-  var devicePre = false;
-
-  if (devicePre) {
-    runApp(DevicePreview(
-      //enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
-    ));
-  } else {
-    runApp(const MyApp());
-  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      // locale: DevicePreview.locale(context),
-      // builder: DevicePreview.appBuilder,
       title: 'Choco',
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
